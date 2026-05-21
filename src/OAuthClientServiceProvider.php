0bd7ca940ccc22b87d50ad31a18964c08bc2681e
Carl <52984331+carlcalma@users.noreply.github.com>
2026-05-21 15:44:33 +0800

    [SC-44] feat: add OAuth callback controller (#1) (#2)

--- src/OAuthClientServiceProvider.php
+++ src/OAuthClientServiceProvider.php
@@ -5,23 +5,21 @@ namespace SmartCampus\OAuthClient;
 use Illuminate\Support\ServiceProvider;
 use SmartCampus\OAuthClient\Http\Middleware\ValidateOAuthToken;
 use SmartCampus\OAuthClient\Services\OAuthService;
-use SmartCampus\OAuthClient\Services\RolePermissionSyncService;
 
 class OAuthClientServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        $this->mergeConfigFrom(__DIR__ . '/../config/oauth-client.php', 'oauth-client');
+        $this->mergeConfigFrom(__DIR__.'/../config/oauth-client.php', 'oauth-client');
 
         $this->app->singleton(OAuthService::class);
-        $this->app->singleton(RolePermissionSyncService::class);
     }
 
     public function boot(): void
     {
         if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__ . '/../config/oauth-client.php' => config_path('oauth-client.php'),
+                __DIR__.'/../config/oauth-client.php' => config_path('oauth-client.php'),
             ], 'oauth-client-config');
         }
 
